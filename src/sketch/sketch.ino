69294da240dc6fe2d84f4f03995e07c7f6394454
DirtyCowTeet <crasor@pdx.edu>
2024-11-13 12:37:32 -0800

    Update sketch.ino
    integrated cloud and oled files into one

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,7 +1,12 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "config.h"
+#include "AdafruitIO_WiFi.h"
 
 // Define the OLED display dimensions
 #define SCREEN_WIDTH 128
@@ -11,48 +16,37 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); // -1 for no reset pin
 
 
+//Create Adafruit IO instances for uploading to the cloud
+AdafruitIO_Feed *voltage = io.feed("Voltage");
+AdafruitIO_Feed *current = io.feed("Current");
+AdafruitIO_Feed *power = io.feed("Power");
+
 // Create an INA219 instance
 Adafruit_INA219 ina219;
 
-void setup() {
-
-  // Initialize the display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Address 0x3C for 128x64
-  display.clearDisplay();                       // Clear the buffer
-
-  // Set text color
-  display.setTextColor(SSD1306_WHITE);
-  
-  Serial.begin(115200);
-  // Initialize the INA219
-  if (!ina219.begin()) {
-    Serial.println("Could not find a valid INA219 sensor, check wiring!");
-    while (1);
+// Gets MAC Address for the esp32, This function is needed
+// If you want to communicate with PSU's IoT Network
+// Head over to config.h if you want to connect to PSU IoT.
+void getMACAddress()
+{
+  uint8_t baseMACArr[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMACArr);
+  if (ret == ESP_OK){
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMACArr[0], baseMACArr[1], baseMACArr[2],
+                  baseMACArr[3], baseMACArr[4], baseMACArr[5]);
+                  
+  }
+  else{
+    Serial.println("... I couldn't read it...");
   }
 }
 
-void loop() {
-  // Read voltage (in volts)
-  float voltage = ina219.getBusVoltage_V();
-  // Read current (in milliamps)
-  float current = ina219.getCurrent_mA();
-  
-  // Read power (in milliwatts)
-  float power = ina219.getPower_mW();
-  
-  // Print the results to the Serial Monitor
-  Serial.print("Bus Voltage: ");
-  Serial.print(voltage);
-  Serial.println(" V");
-  
-  Serial.print("Current: ");
-  Serial.print(current);
-  Serial.println(" mA");
-  
-  Serial.print("Power: ");
-  Serial.print(power);
-  Serial.println(" mW");
+void send_data_to_screen(float power){
 
+  //get updated power value each time
+  power = ina219.getPower_mW();
+  
   //clear display
   display.clearDisplay();
   
@@ -71,6 +65,83 @@ void loop() {
   // Send the buffer to the display
   display.display();
 
-  // Wait 1 second before the next reading
-  delay(1000);
+  delay(100);
+}
+
+void setup() {
+
+  // Initialize the display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Address 0x3C for 128x64
+  display.clearDisplay();                       // Clear the buffer
+
+  // Set text color
+  display.setTextColor(SSD1306_WHITE);
+  
+  Serial.begin(115200);
+
+  // Wait for serial monitor to open
+  while(! Serial);
+  Serial.println("Hello World!");
+
+  // Initialize Wifi Service
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+
+  // Get the MAC Address. This should run regardless if you have internet or not...
+  Serial.print("Your ESP32's MAC address is: ");
+  getMACAddress();
+
+  // Connect to Adafruit IO
+  Serial.print("Attempting Connection with Adafruit IO");
+  io.connect();
+
+  // Check if connection was successful
+  while(io.status() < AIO_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+
+  // Initialize INA219
+  if (!ina219.begin()){
+    Serial.println("Unable to Initialize INA 219 Library");
+  while(1);
+  }
+
+  // Connection Successful
+  Serial.println(io.statusText());
+}
+
+void loop() {
+  // Fetch values from the INA219
+  float voltageValue = ina219.getBusVoltage_V();
+  float currentValue = ina219.getCurrent_mA();
+  float powerValue = ina219.getPower_mW();
+
+  // Print the results to the Serial Monitor
+  //Serial.print("Bus Voltage: ");
+  //Serial.print(voltageValue);
+  //Serial.println(" V");
+
+  //Serial.print("Current: ");
+  //Serial.print(currentValue);
+  //Serial.println(" mA");
+
+  Serial.print("Power: ");
+  Serial.print(powerValue);
+  Serial.println(" mW");
+
+  for (int i = 0; i < 20; ++i){
+    send_data_to_screen(powerValue);
+  }
+
+  // Send the results to Adafruit IO cloud
+  io.run();
+
+  // Save the readings to the Adafruit IO feeds 0
+  Serial.println("");
+  //voltage->save(String(voltageValue));
+  //current->save(String(currentValue));
+  power->save(String(powerValue));
+
+  //delay(2000); // Wait for 2 seconds before the next reading (reduces throttle)
 }
